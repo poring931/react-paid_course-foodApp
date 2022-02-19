fd4ec23b8a2b6827204e16e24d520794a0736891
poring931 <poring.m@mail.ru>
2022-02-19 22:40:01 +0300

    база для будущего приложения

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header/>
+    
+
+          {/* <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="contacts" element={<Contacts />} />
+              <Route path="tick-tack-toe" element={<TickTackToe />} />
+              <Route path="*" element={<NotFound />} />
+          </Routes> */}
+
+      <Footer/>
+    </>
   );
 }
 
